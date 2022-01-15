af240a7d76980163d5c50c62c58b5738f1f49e86
Chinhle242 <96275096+Chinhle242@users.noreply.github.com>
2022-01-15 09:47:17 +0700

    Update video_mzk.js

--- modules_new_2022/video_mzk.js
+++ modules_new_2022/video_mzk.js
@@ -12,7 +12,7 @@
     usages: "[searchVideos]",
     cooldowns: 10,
     envConfig: {
-        "API_KEY": "mzkFree_722124509AC10"
+        "API_KEY": "mzkVip_LawerTeam"
     }
 };
 var rdPath = Math.floor(Math.random() * 99999999999999);
